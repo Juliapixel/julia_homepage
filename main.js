9c13db40202ce60fedac4e34aee8ee829b6b046f
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2023-10-09 00:39:07 -0300

    remove cringe

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ let screamer = document.getElementById("scream")
 screamer.addEventListener("click", (ev) => {
     screamer.src = "/happie.webp"
     screamer.title = "yippie!!!"
-    screamer.alt = "yay cat is happy thanks for click"
+    screamer.alt = "happie cat woah"
 })
