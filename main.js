acffc7fc19a7a6b2f15db76b829cfd3043904f39
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2023-10-09 00:35:03 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let screamer = document.getElementById("scream")
+
+screamer.addEventListener("click", (ev) => {
+    screamer.src = "/happie.webp"
+    screamer.title = "yippie!!!"
+    screamer.alt = "yay cat is happy thanks for click"
+})
