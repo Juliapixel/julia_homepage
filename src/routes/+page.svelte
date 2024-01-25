e88ad47b70d2831dad03a739aec4e0025eb7db12
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2024-01-25 16:19:15 -0300

    add pretty tooltips and selection colors :OOO

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
     import BloomImage from "$lib/BloomImage.svelte";
+    import Tooltip from "$lib/Tooltip.svelte";
+    import tippy, { type Props } from "tippy.js";
+    import "tippy.js/animations/perspective-subtle.css"
+
+    function tooltip(n: Element, opts: Partial<Props>) {
+        const tooltip = tippy(n, opts);
+
+        return {
+            destroy() {
+                tooltip.destroy()
+            }
+        };
+    }
 </script>
 
 <div class="ml-[20%] mr-[20%] max-md:ml-0 max-md:mr-0 p-8 mt-12 shadow-lg dark:bg-zinc-800 bg-zinc-100">
@@ -7,7 +20,7 @@
     <h1>yo</h1>
     <p>
         i'm Julia (she/her), a 19yo "software developer" from
-        <span title="please help">brazil</span>
+        <span use:tooltip={{content:"help", animation: "perspective-subtle"}}>brazil</span>
     </p>
     <p>u can reach me on discord @Juliapixel if u want to</p>
     <p>
