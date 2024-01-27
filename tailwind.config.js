af9916b200a590e8d4a47073207cbfaf5a80dfce
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2024-01-27 02:24:08 -0300

    use system monospace font as fallback

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
+        fontFamily: {
+            // "sans": ["vecterminus14", "ui-monospace", "monospace"],
+            "mono": ["vecterminus14", "ui-monospace", "monospace"],
+            // "body": ["vecterminus14", "ui-monospace", "monospace"]
+        },
         extend: {
             fontFamily: {
                 vecTerminus: ["vecterminus14"]
