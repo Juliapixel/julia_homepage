11e41de668bd08f9b5c7d6433f75385441eee5f7
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2024-01-31 01:49:51 -0300

    move current i18n keys to "home" namespace

--- src/routes/(group)/+page.svelte
+++ src/routes/(group)/+page.svelte
@@ -16,20 +16,20 @@
 </script>
 
 <BloomImage src="/uuh.webp" alt="epic cat!!!1!"/>
-<h1>{$_("yo")}</h1>
+<h1>{$_("home.yo")}</h1>
 <p>
-    {$_("intro_main")}
-    <span use:tooltip={{content:"help", animation: "perspective-subtle"}}>{$_("brazil")}</span>
+    {$_("home.intro_main")}
+    <span use:tooltip={{content:"help", animation: "perspective-subtle"}}>{$_("home.brazil")}</span>
 </p>
-<p>{$_("reach_on_discord")}</p>
+<p>{$_("home.reach_on_discord")}</p>
 <p>
-    {$_("or_by")}
+    {$_("home.or_by")}
     <a href="mailto://homepage@juliapixel.com">email</a>
-    {$_("that_sorta_stuff")}
+    {$_("home.that_sorta_stuff")}
 </p>
 <p>
-    {$_("check_out_my")}
+    {$_("home.check_out_my")}
     <a href="https://github.com/juliapixel/">github</a>
-    {$_("woah")}
+    {$_("home.woah")}
 </p>
-<p class="mt-32">{$_("adding_more_stuff")}</p>
+<p class="mt-32">{$_("home.adding_more_stuff")}</p>
