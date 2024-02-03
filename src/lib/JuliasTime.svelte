9d94ca16d5220f1009afa7d18cd6339f79b8a6ae
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2024-02-03 00:53:16 -0300

    WHY are getUTCMonth() and getUTCDay() zero-based while getUTCDate() isn't????

--- src/lib/JuliasTime.svelte
+++ src/lib/JuliasTime.svelte
@@ -7,13 +7,13 @@
     onMount(() => {
         const interval = setInterval(() => {
             time = new Date();
-            time.setUTCMilliseconds(time.getUTCMilliseconds() - (3600 * 1000 * 3));
+            time.setUTCMilliseconds(new Date().getUTCMilliseconds() - (3600 * 1000 * 3));
         }, 1000)
 
         return () => clearInterval(interval);
     })
 </script>
 
-{`${time.getUTCFullYear()}-${time.getUTCMonth().toString().padStart(2, "0")}-${time.getUTCDay().toString().padStart(2, "0")}`}
+{`${time.getUTCFullYear()}-${(time.getUTCMonth() + 1).toString().padStart(2, "0")}-${time.getUTCDate().toString().padStart(2, "0")}`}
 {`${time.getUTCHours().toString().padStart(2, "0")}:${time.getUTCMinutes().toString().padStart(2, "0")}:${time.getUTCSeconds().toString().padStart(2, "0")}`}
 UTC-3
