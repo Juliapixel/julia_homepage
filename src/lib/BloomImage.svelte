6a9835304f76f56c353395ea57fea3d587367165
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2024-03-19 21:25:23 -0300

    make bloom images actually work consistently

--- src/lib/BloomImage.svelte
+++ src/lib/BloomImage.svelte
@@ -8,13 +8,15 @@
     let main_image: HTMLImageElement;
     let bloom_image: HTMLImageElement;
 
-    onMount(() => {
+    function add_bloom() {
+        // SUUUURELY bloom_image won't be undefined sometimes right?
+        // race conditions are not real and can't hurt you
         bloom_image.width = main_image.width;
         bloom_image.height = main_image.height;
-    });
+    };
 </script>
 
 <div>
     <img {src} {alt} bind:this={bloom_image} class="{clazz} pointer-events-none absolute mix-blend-screen blur-md z-[1] opacity-50"/>
-    <img {src} {alt} bind:this={main_image} class="{clazz} relative"/>
+    <img {src} {alt} bind:this={main_image} on:load={add_bloom} class="{clazz} relative"/>
 </div>
