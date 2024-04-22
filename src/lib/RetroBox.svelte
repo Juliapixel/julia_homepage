cdb84077899cfce0dae3a70c8e3e0dc53f7f0f78
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2024-04-22 01:23:20 -0300

    window button styling

--- src/lib/RetroBox.svelte
+++ src/lib/RetroBox.svelte
@@ -6,10 +6,10 @@
 <div class="p-[2px] border-4 border-r-neutral-700 border-b-neutral-700 bg-neutral-400">
     <div class="flex flex-row justify-between items-center pl-2 m-1 h-8 from-blue-900 to-blue-700 bg-gradient-to-r">
         <span>{title}</span>
-        <div class="flex flex-row h-full">
-            <img src="uuh.webp" class="h-full"/>
-            <img src="uuh.webp" class="h-full"/>
-            <img src="uuh.webp" class="h-full"/>
+        <div class="flex flex-row h-full p-1 gap-1">
+            <img src="uuh.webp" class="h-full aspect-square"/>
+            <img src="uuh.webp" class="h-full aspect-square"/>
+            <img src="uuh.webp" class="h-full aspect-square"/>
         </div>
     </div>
     <main class="p-8 mt-2 border-4 border-t-neutral-700 border-l-neutral-700 m-1 bg-neutral-800">
