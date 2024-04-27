33eb99f3014555eca5b91610f36142aa707ef656
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2024-04-26 23:25:28 -0300

    add margin to top of retro box

--- src/routes/(group)/+layout.svelte
+++ src/routes/(group)/+layout.svelte
@@ -44,7 +44,7 @@
         <DarkToggle dark_enabled={data.is_dark}/>
     </svelte:fragment>
 </Header>
-<div class="lg:mx-[20%]">
+<div class="lg:mx-[20%] mt-8">
     <RetroBox title="/usr/bin/homepage {fakeParams($page.url)}{$page.url.pathname}" bottom_text="</> with <3 by Julia">
         <slot />
     </RetroBox>
