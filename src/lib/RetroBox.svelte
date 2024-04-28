dc054f951e284813756517ac766b2e04332eedfa
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2024-04-28 01:01:35 -0300

    actually add the window buttons

--- src/lib/RetroBox.svelte
+++ src/lib/RetroBox.svelte
@@ -8,9 +8,15 @@
     <div class="flex flex-row justify-between items-center pl-2 m-1 h-8 from-blue-900 to-blue-700 bg-gradient-to-r">
         <span>{title}</span>
         <div class="flex flex-row h-full p-1 gap-1">
-            <img src="/uuh.webp" class="h-full aspect-square"/>
-            <img src="/uuh.webp" class="h-full aspect-square"/>
-            <img src="/uuh.webp" class="h-full aspect-square"/>
+            <button class="p-0 max-w-fit" disabled aria-hidden="true">
+                <img src="/minimize.png" alt="minimize icon" class="h-full aspect-square [image-rendering:crisp-edges]" />
+            </button>
+            <button class="p-0 max-w-fit" disabled aria-hidden="true">
+                <img src="/maximize.png" alt="maximize icon" class="h-full aspect-square [image-rendering:crisp-edges]" />
+            </button>
+            <button class="p-0 max-w-fit" disabled aria-hidden="true">
+                <img src="/close.png" alt="close icon" class="h-full aspect-square [image-rendering:crisp-edges]" />
+            </button>
         </div>
     </div>
     <main class="p-8 mt-2 border-4 border-t-neutral-700 border-l-neutral-700 m-1 bg-neutral-800">
